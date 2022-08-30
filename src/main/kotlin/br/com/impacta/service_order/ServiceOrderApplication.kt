2c2816c0a1063f5699bfe29c5fa75274db777b6b
pmagalhaes2 <patricia.magalhaes2@gmail.com>
2022-08-30 16:55:18 -0300

    initial commit

---
+++ src/main/kotlin/br/com/impacta/service_order/ServiceOrderApplication.kt
@@ -0,0 +1,11 @@
+package br.com.impacta.service_order
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServiceOrderApplication
+
+fun main(args: Array<String>) {
+	runApplication<ServiceOrderApplication>(*args)
+}
