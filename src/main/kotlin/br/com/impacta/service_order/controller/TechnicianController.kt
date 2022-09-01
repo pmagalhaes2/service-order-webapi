4c66c87c3828f5004ff3f910e28511fbffb1c0cc
pmagalhaes2 <patricia.magalhaes2@gmail.com>
2022-09-01 19:33:19 -0300

    fix: add transactional annotation and remove wilcard import

--- src/main/kotlin/br/com/impacta/service_order/controller/TechnicianController.kt
+++ src/main/kotlin/br/com/impacta/service_order/controller/TechnicianController.kt
@@ -5,7 +5,14 @@ import br.com.impacta.service_order.requests.TechnicianCreationRequest
 import br.com.impacta.service_order.requests.TechnicianUpdateRequest
 import br.com.impacta.service_order.services.TechnicianService
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
 import javax.transaction.Transactional
 import javax.validation.Valid
 
