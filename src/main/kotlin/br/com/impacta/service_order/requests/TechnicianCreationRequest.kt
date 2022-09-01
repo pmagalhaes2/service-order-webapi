17c3419519ac7086d6f45a71c4d9265a200c704b
pmagalhaes2 <patricia.magalhaes2@gmail.com>
2022-09-01 17:46:29 -0300

    chore: create dto for technician creation response

---
+++ src/main/kotlin/br/com/impacta/service_order/requests/TechnicianCreationRequest.kt
@@ -0,0 +1,23 @@
+package br.com.impacta.service_order.requests
+
+import org.hibernate.validator.constraints.br.CPF
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.Pattern
+
+data class TechnicianCreationRequest(
+    val id: Int?,
+
+    @field:NotEmpty(message = "O campo NOME é requerido")
+    @field:Pattern(
+        regexp = "^[\\pL\\pM\\p{Zs}]+\$",
+        message = "O campo NOME deve conter apenas letras"
+    )
+    val name: String,
+
+    @field:NotEmpty(message = "O campo CPF é requerido")
+    @field:CPF(message = "Número de CPF inválido")
+    val cpf: String,
+
+    @field:NotEmpty(message = "O campo TELEFONE é requerido")
+    val phoneNumber: String,
+)
